16115a69ebdba9f2dee11eeab76fa68d9f334269
IlanVaks <ilanvaks@gmail.com>
2023-04-04 15:10:22 -0400

    FIRST phase

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 3003;
+
+const app = express();
+app.use( cors() );
+app.use( express.json() );
+
+app.listen(PORT => {
+  console.log(`Listening on http://localhost:${PORT}`)
+})
+
